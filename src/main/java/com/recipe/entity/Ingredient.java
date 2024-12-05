9642d0aedfbdf568f4b3d937b7b9f27f8d5b34f7
so040602 <so040602@gmail.com>
2024-12-05 17:47:12 +0900

    김창우/식재료테이블 추가

---
+++ src/main/java/com/recipe/entity/Ingredient.java
@@ -0,0 +1,24 @@
+package com.recipe.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ingredient {
+    @Id
+    @Column(name = "Ingredient_idx")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ingredient_idx;
+
+    private String ingredient;
+    private String image;
+
+
+}
