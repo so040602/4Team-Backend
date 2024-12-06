be8c6521b41d12764189ad4af95e428d1e4d43ad
leejongchan89 <cleejy89@icloud.com>
2024-12-06 16:14:09 +0900

    Member 엔티티 테이블 추가

---
+++ src/main/java/com/recipe/entity/Member.java
@@ -0,0 +1,37 @@
+package com.recipe.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long member_id;
+    private String social_id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+    private String auth_provider;
+    private String gender;
+    private Date birth;
+    private Long mobile;
+    private String nick_name;
+    private String profile_image;
+    private Timestamp regdate;
+    private Timestamp moddate;
+
+}
